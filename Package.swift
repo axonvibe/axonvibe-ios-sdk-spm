15a23e3d2b8ecb35c63ab7fdfcd61a9613bee9ed
John McKerrell <john.mckerrell@axonvibe.com>
2021-07-21 17:09:23 +0100

    Switch to using variables in package.swift, add license

--- Package.swift
+++ Package.swift
@@ -1,6 +1,9 @@
 // swift-tools-version:5.4.0
 import PackageDescription
 
+let version = "17.1.1"
+let checksum = "8263e5881cf874daec03cfd8e9360e41469caeb374f58f634075e897204f9450"
+
 let package = Package(
     name: "VibeSDK",
     platforms: [
@@ -18,8 +21,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "VibeSDK",
-            url: "https://axonvibe.jfrog.io/artifactory/ios-sdk-release/VibeSDK/17.1.1/VibeSDK.zip",
-            checksum: "8263e5881cf874daec03cfd8e9360e41469caeb374f58f634075e897204f9450"
+            url: "https://axonvibe.jfrog.io/artifactory/ios-sdk-release/VibeSDK/\(version)/VibeSDK.zip",
+            checksum: checksum
         )
     ]
 )
