121c19a1b9c3aef15daa3256aa792b7477dd1fbb
Bassel Ezzeddine <bassel.ezzeddine@axonvibe.com>
2024-02-09 17:09:54 +0000

    17.22.1

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
-// swift-tools-version:5.3.0
+// swift-tools-version: 5.3.0
 import PackageDescription
 
-let version = "17.22.0"
-let checksum = "84b3f762b6830d9f561b757aaf8a37cbbcb17d2dbd96db79c678daa3c469218d"
+let version = "17.22.1"
+let checksum = "8bc48f16443beed2504c0954c303a1ef21a81ff4ef911ad608da75f75536ec7e"
 
 let package = Package(
     name: "VibeSDK",
@@ -10,7 +10,6 @@ let package = Package(
         .iOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "VibeSDK",
             targets: ["VibeSDK"]
