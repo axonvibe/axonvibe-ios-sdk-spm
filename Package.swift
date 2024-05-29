15ba8ca1770260a7635893598697e8482fb27332
Bassel Ezzeddine <bassel.ezzeddine@axonvibe.com>
2024-05-29 21:58:25 +0000

    17.25.0

--- Package.swift
+++ Package.swift
@@ -1,8 +1,8 @@
 // swift-tools-version: 5.3.0
 import PackageDescription
 
-let version = "17.24.0"
-let checksum = "7b01b86ddd469f6352bc039e2ff657cbc32fcd5b65756a516788d5d05e4ed9fe"
+let version = "17.25.0"
+let checksum = "82da424b00962530156854b9404f2b47550d85ed623104cd54d9b8c2ad950f41"
 
 let package = Package(
     name: "VibeSDK",
